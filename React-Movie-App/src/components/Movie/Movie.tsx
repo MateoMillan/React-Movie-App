9311cbf02500aa5f359c099ee761833705da779e
MateoMillan <mmillan@iico.vippinn.com>
2023-10-24 11:55:32 -0300

    movie-row

--- React-Movie-App/src/components/Movie/Movie.tsx
+++ React-Movie-App/src/components/Movie/Movie.tsx
@@ -1,7 +1,17 @@
-import "./Movie.css"
+import { Link } from "react-router-dom";
+import "./Movie.css";
 
-export default function Movie({movie}: {movie: Movie}) {
-	return <div className="movie">
-		<img src={movie.Poster} alt={movie.Title} />
-	</div>
-} 
+export default function Movie({ movie }: { movie: Movie }) {
+	return (
+		<div className="movie">
+			<Link to={`/movie/${movie.imdbID}`}>
+				<img
+					src={movie.Poster}
+					alt={movie.Title}
+					className="movie-image"
+					draggable="false"
+				/>
+			</Link>
+		</div>
+	);
+}
