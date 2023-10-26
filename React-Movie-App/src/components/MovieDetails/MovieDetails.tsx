f1a350454a5404903411fcbd52398b3e35da9a9e
MateoMillan <mmillan@iico.vippinn.com>
2023-10-26 15:09:42 -0300

    movie-details-re-estilizado

--- React-Movie-App/src/components/MovieDetails/MovieDetails.tsx
+++ React-Movie-App/src/components/MovieDetails/MovieDetails.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import "./MovieDetails.css";
 import { useParams } from "react-router";
-import Details from "./Details/Details";
 import Ratings from "./Ratings/Ratings";
 
 export default function MovieDetails() {
 	const { imdbID } = useParams();
 	const [movie, setMovie] = useState<MovieInfo>();
-	const [rated, setRated] = useState<string>();
 	function getMovieData() {
 		fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=28435cd`)
 			.then((response) => {
@@ -26,64 +24,44 @@ export default function MovieDetails() {
 
 	useEffect(() => {
 		getMovieData();
-		switch (movie?.Rated) {
-			case "G":
-				setRated(
-					"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/MPA_G_RATING_%28block%29.svg/1920px-MPA_G_RATING_%28block%29.svg.png"
-				);
-				break;
-			case "PG":
-				setRated(
-					"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/MPA_PG_RATING_%28block%29.svg/1920px-MPA_PG_RATING_%28block%29.svg.png"
-				);
-				break;
-			case "PG-13":
-				setRated(
-					"https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/MPA_PG-13_RATING_%28block%29.svg/1920px-MPA_PG-13_RATING_%28block%29.svg.png"
-				);
-				break;
-			case "R":
-				setRated(
-					"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/MPA_R_RATING_%28block%29.svg/1920px-MPA_R_RATING_%28block%29.svg.png"
-				);
-				break;
-			case "NC-17":
-				setRated(
-					"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/MPA_NC-17_RATING_%28block%29.svg/1920px-MPA_NC-17_RATING_%28block%29.svg.png"
-				);
-				break;
-		}
 	}, [movie]);
 
 	return (
 		movie && (
 			<div className="movie-details">
+				<div className="title-details">
+					<h1 className="name-details">My Movie App</h1>
+				</div>
+				<img
+					src={movie.Poster}
+					alt={
+						movie.Title +
+						movie.Actors +
+						movie.Country +
+						movie.Genre +
+						movie.Writer
+					}
+					className="details-image"
+				/>
+				<div className="details-gradient"></div>
 				<div className="top">
-					<div className="left">
-						<img
-							src={movie.Poster}
-							alt={
-								movie.Title +
-								movie.Actors +
-								movie.Country +
-								movie.Genre +
-								movie.Writer
-							}
-							className="details-image"
-						/>
-					</div>
 					<div className="right">
-						<Details details={movie} />
+						<h1 className="movie-title">{movie.Title}</h1>
+						<div className="details">
+							<div className="detail">{movie.Runtime}</div>
+							<div className="detail">{movie.Rated}</div>
+							<div className="detail">{movie.Year}</div>
+						</div>
+						<p className="plot">{movie.Plot}</p>
 					</div>
 				</div>
 				<div className="bottom">
-					<h2>Ratings</h2>
-					<div className="ratings">
-						<Ratings ratings={movie.Ratings} />
+					<div className="ratings-container">
+						<h2>Ratings</h2>
+						<div className="ratings">
+							<Ratings ratings={movie.Ratings} />
+						</div>
 					</div>
-				</div>
-				<div className="rated">
-					<img src={rated} alt="" className="rated-image" />
 				</div>
 			</div>
 		)
